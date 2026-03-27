0ce11b90544bcc2f31a1e368988f3a1166385c1d
Shaun <skichenbrand@gmail.com>
2026-03-27 10:30:20 +0200

    Added libraryComponents exports

--- src/library.js
+++ src/library.js
@@ -4,6 +4,9 @@ import { CodeEditor } from './components/codeEditor';
 //PropSpecs
 import propsCodeEditor from './components/codeEditor/props';
 
+//Components
+export * from './libraryComponents';
+
 import { registerComponentTypes } from '@intenda/opus-ui';
 
 registerComponentTypes([{
